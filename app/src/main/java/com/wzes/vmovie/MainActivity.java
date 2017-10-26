74246b7eaa2509588d169ef7162ac433a9a4fae9
wzes <793890838@qq.com>
2017-10-26 12:41:59 +0800

    create the movie app

---
+++ app/src/main/java/com/wzes/vmovie/MainActivity.java
@@ -0,0 +1,13 @@
+package com.wzes.vmovie;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
